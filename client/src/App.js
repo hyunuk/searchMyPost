83c818d2e6c63feca0d9f41221ea5903815c8329
Hyunuk <dev.hyunuk@gmail.com>
2021-07-18 21:41:21 -0700

    feat: implement login w/FB on client

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 import { useEffect } from 'react'
+import FacebookLogin from "react-facebook-login";
 import './App.css';
+
+const responseFacebook = (response) => {
+  console.log(response);
+}
 
 function App() {
   const callApi = async () => {
@@ -10,7 +15,14 @@ function App() {
   useEffect(() => {
     callApi();
   }, []);
-  return <div>test</div>
+  return <div>
+    <FacebookLogin
+      appId={process.env.REACT_APP_FACEBOOK_APP_ID}
+      autoLoad={false}
+      fields="name,email,picture"
+      callback={responseFacebook}
+    />
+  </div>
 }
 
 export default App;

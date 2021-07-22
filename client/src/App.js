3f16d9c65bcc16c26f6b6b8c669b3f3f9cf635cc
Hyunuk <dev.hyunuk@gmail.com>
2021-07-22 00:32:22 -0700

    feat: wip for setting up the FE

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,59 @@
 import axios from 'axios'
-import React, { useEffect } from 'react'
-import FacebookLogin from 'react-facebook-login'
+import React, { useEffect, useRef } from 'react'
+import { useHistory } from 'react-router-dom'
+import {
+    Button, Paper, IconButton, InputBase,
+} from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
+
 import './App.css'
 
-const responseFacebook = response => {
-    console.log(response)
-}
-
 function App() {
-    const callApi = async () => {
-        axios.get('/').then(res => console.log(res))
+    const input = useRef(null)
+    const history = useHistory()
+    const handleSubmit = () => {
+        axios.get(`/posts/search/?q=${input.current?.value}`).then(res => console.log(res))
     }
 
-    useEffect(() => {
-        callApi()
-    }, [])
+    const connectFB = async () => {
+        try {
+            await axios.get('/auth/login/facebook')
+            history.push(`/${user.username}`)
+        } catch (err) {
+            setOpen(true)
+        }
+    }
+
+    const ContainedButton = ({ title, ...props }) => (
+        <Button
+            variant="contained"
+            color="primary"
+            disableElevation
+            {...props}
+        >
+            {title}
+        </Button>
+    )
+
     return (
-        <div>
-            <FacebookLogin
-                appId={process.env.REACT_APP_FACEBOOK_APP_ID}
-                autoLoad={false}
-                fields="name,email,picture"
-                callback={responseFacebook}
+        <Paper component="form" onSubmit={handleSubmit}>
+            <InputBase
+                style={{
+                    marginLeft: '.5rem',
+                    flex: 1,
+                    fontSize: 'small',
+                }}
+                placeholder="Search"
+                inputRef={input}
+                inputProps={{ 'aria-label': 'search google maps' }}
             />
-        </div>
+            <IconButton type="submit" size="small" aria-label="search">
+                <SearchIcon />
+            </IconButton>
+            <a href="/auth/login/facebook">
+                <ContainedButton title="Login with Facebook" />
+            </a>
+        </Paper>
     )
 }
 
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,5 +4,11 @@ import { getPosts } from '../controllers/posts.js'
 const router = express.Router()
 
 router.get('/', getPosts)
+router.get('/search', getSearch)
 
 export default router
+
+
+// C -> B "fb.com/posts/search?q='alice'"
+// B -> C "res = {searchResult, pagetoken, until}"
+// C -> B "fb.com/posts/search?q='alice'&pagetoken=''&until=''"
